b6bb170a74948de8fc50eda629d4f090a42ef1f6
unknown <armandodejesus678@gmail.com>
2019-11-08 18:59:19 -0600

    creacion de server render listo

--- source/server_render.js
+++ source/server_render.js
@@ -1,9 +1,23 @@
+import React from 'react';
 import { Router } from 'express';
-
+import asyncRenderToString from 'react-fetch-ssr';
+import { StaticRouter } from "react-router";
+import App from './app/app'
+import { renderToStaticMarkup } from 'react-dom/server';
 const router = Router();
+import Markup from './markup'
+router.get('*', async (request, response) => {
+	
+	const context = {};
+	const content_rendered = await asyncRenderToString(
+		<StaticRouter location={request.url} context={context}>
+			<App />
+		</StaticRouter>
+	);
 
-router.get('*', (request, response) => {
-	response.send('hello render to string...');
+	const html = renderToStaticMarkup(<Markup content_rendered={content_rendered} />);
+	
+	response.send(html);
 });
 
 export default router;
