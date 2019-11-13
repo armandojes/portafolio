ca73079bde752d1c740ae9987ae6d15d014a2a55
unknown <armandodejesus678@gmail.com>
2019-11-13 13:04:18 -0600

    header listo

--- source/app/components/header/components/menu/index.jsx
+++ source/app/components/header/components/menu/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import style from './style';
 import useSelector from '../../../../hooks/selector';
-import { Link } from 'react-router-dom';
+import { NavLink  , useLocation } from 'react-router-dom';
 
 function Menu (){
 	
 
 	const items_menu = useSelector(state => state.meta.menu);
-	
-	console.log('render_menu')
-	
+
 	return (
 		<div className={style.menu_content}>
 			{items_menu.map(menu => 
-				<Link to={menu.path} key={menu.text}>{menu.text}</Link>
+				<NavLink   
+					exact={true} 
+					to={menu.path} 
+					activeClassName={style.seleted} 
+					className={style.menu} key={menu.text}>{menu.text} 
+				</NavLink  >
 			)}
 		</div>
 	)
