d43bff3a0db39662b7e5d01786ce1716eee5fedf
Jesusmaster <armandodejesus678@gmail.com>
2019-11-19 12:15:35 -0600

    sections ready

--- source/app/pages/home/components/sections/index.jsx
+++ source/app/pages/home/components/sections/index.jsx
@@ -2,48 +2,55 @@ import React from 'react';
 import style from './style';
 import Container from '../../../../components/container';
 import { Link } from 'react-router-dom';
+import useSelector from 'hooks/selector';
+
 
 function Sections () {
+
+  const language = useSelector( state => state.language);
+
   return (
     <section className={style.wrapper}>
       <Container>
 
 
         <div className={`${style.element} ${style.normal}`}>
-          <div className={style.picture_layer} style={{backgroundImage: `url(${ASSETS}/pictures/picture.png)`}} />
+          <div className={style.picture_layer} style={{backgroundImage: `url(${ASSETS}/pictures/abilities.jpg)`}} />
           <div className={style.description}>
-            <h3>Habilidades</h3>
-            <p>
-              Maquetacion, Diseño, Prototypo, Desarollo de aplicaciones web interactivas con las ultimas tecnologias, SPA, Apps isomorficas... 
-              <Link to="/abilities" className={style.link_read_more}>Conocer mas...</Link>
-            </p>
+            <h3>{language === 'es' ? 'Habilidades' : 'Skills'}</h3>
+            {language === 'es' && (
+              <p>
+                Maquetacion, Diseño, Prototypo, Desarollo de aplicaciones web interactivas con las ultimas tecnologias, SPA, Apps isomorficas... 
+                <Link to="/abilities" className={style.link_read_more}>Conocer mas...</Link>
+              </p>
+            )}
+            {language === 'us' && (
+              <p>
+                Layout, Design, Prototypo, Development of interactive web applications with the latest technologies, SPA, Isomorphic Apps ... 
+                <Link to="/abilities" className={style.link_read_more}>Read more...</Link>
+              </p>
+            )}
           </div>
         </div>
         
         <div className={`${style.element} ${style.reverse}`}>
-          <div className={style.picture_layer} style={{backgroundImage: `url(${ASSETS}/pictures/picture.png)`}} />
+          <div className={style.picture_layer} style={{backgroundImage: `url(${ASSETS}/pictures/proyects.jpg)`}} />
           <div className={style.description}>
-            <h3>Habilidades</h3>
-            <p>
-              Maquetacion, Diseño, Prototypo, Desarollo de aplicaciones web interactivas con las ultimas tecnologias, SPA, Apps isomorficas... 
-              <Link to="/abilities" className={style.link_read_more}>Conocer mas...</Link>
-            </p>
+          <h3>{language === 'es' ? 'Proyectos' : 'Projects'}</h3>
+            {language === 'es' && (
+              <p>
+                Conoce mis proyectos, Tiendas online, Aplicaciones de escritorio, Utilidades y librerias de codigo... 
+                <Link to="/abilities" className={style.link_read_more}>Conocer mas...</Link>
+              </p>
+            )}
+            {language === 'us' && (
+              <p>     
+                Know my projects, Online stores, Desktop applications, Utilities and code libraries ...
+                <Link to="/abilities" className={style.link_read_more}>Read more...</Link>
+              </p>
+            )}
           </div>
         </div>
-        
-        <div className={`${style.element} ${style.normal}`}>
-          <div className={style.picture_layer} style={{backgroundImage: `url(${ASSETS}/pictures/picture.png)`}} />
-          <div className={style.description}>
-            <h3>Habilidades</h3>
-            <p>
-              Maquetacion, Diseño, Prototypo, Desarollo de aplicaciones web interactivas con las ultimas tecnologias, SPA, Apps isomorficas... 
-              <Link to="/abilities" className={style.link_read_more}>Conocer mas...</Link>
-            </p>
-          </div>
-        </div>
-        
-        
-
       </Container>
     </section >
   )
