e1c59458325c0f62008c895268de60f3d457dcaf
Jesusmaster <armandodejesus678@gmail.com>
2019-12-05 17:35:29 -0600

    integracion de taringa vue

--- source/app/initial_state/projects/index.jsx
+++ source/app/initial_state/projects/index.jsx
@@ -1,9 +1,11 @@
 import platzi_blog from './platzi';
 import pcl from './pcl';
+import taringa from './taringa';
 
 const projects = {
   platzi_blog,
   pcl,
+  taringa,
 }
 
 export default projects;
---
+++ source/app/initial_state/projects/taringa.js
@@ -0,0 +1,34 @@
+const taringa = {
+  github: 'https://github.com/capitantripa/taringavue',
+  url: 'https://taringavue.now.sh',
+  picture: `${ASSETS}/projects/taringa.png`,
+  title: {es: 'Taringa', us: 'Taringa'},
+  description: {
+    es: 'Aplicación web (SPA) para visualizar posts y Shouts utilizando el API oficial de Taringa.net ',
+    us: 'Web application (SPA) to view posts and shouts using the official Taringa API'
+  },
+  pictures: [
+    `${ASSETS}/projects/taringa_desktop.png`,
+    `${ASSETS}/projects/taringa_shout.png`,
+    `${ASSETS}/projects/taringa_movil.png`,
+  ],
+  objetive: {
+    es: 'Ejercicio de programación para solidificar y practicar conocimientos en una de las librerías más novedosas para el Frontend VUE.js.',
+    us: 'Programming exercise to solidify and practice knowledge in one of the newest libraries for Frontend VUE.js'
+  },
+  details: [
+    {es: 'Interfaces contruido con VueJs', us: 'Interfaces built with VueJs'},
+    {es: 'Soporte para multiples dispositivos (Responsivo)', us: 'Support for multiple devices (Responsive)'},
+    {es: 'Desarrollo basado en componentes', us: 'Component Based Development'},
+    {es: 'Ultima vercion de Javascript transpilado con babel', us: 'Latest version of JavaScript transpile with babel'},
+  ],
+  languages: [
+    'vue',
+    'javascript',
+    'css',
+    'html',
+    'json',
+  ]
+}
+
+export default taringa;
