e2159cb22c65cbf0a5d38cc6aef419284749c203
Jesusmaster <armandodejesus678@gmail.com>
2020-01-08 19:33:54 -0600

    integracion de idiomas

--- source/app/pages/projects/components/react_fetch_ssr/index.jsx
+++ source/app/pages/projects/components/react_fetch_ssr/index.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import style from './style';
+import useSelector from 'hooks/selector';
+
 
 function ReactFetchSsr () {
+
+  const language = useSelector(state => state.language);
+  
   return (
     <div className={style.content} style={{backgroundImage: `url(${ASSETS}/icon/react_gray.png)`}}>
       <div className={style.data}>
         <h1>
           react-fetch-ssr
         </h1>
-        <p>
-        react-fetch-ssr una solución de renderizado de React del lado del servidor, simple, liviana y flexible que abstrae las complejidades del renderizado del lado del servidor. react-fetch-ssr se enfoca únicamente en las peticiones al API, la arquitectura de tu aplicación seguirá estando en tu completo control.
-        </p>
+        {language === 'es' &&(
+          <p>
+          react-fetch-ssr una solución de renderizado de React del lado del servidor, simple, liviana y flexible que abstrae las complejidades del renderizado del lado del servidor. react-fetch-ssr se enfoca únicamente en las peticiones al API, la arquitectura de tu aplicación seguirá estando en tu completo control.
+          </p>
+        )}
+        {language === 'us' && (
+          <p>
+            react-fetch-ssr a server-side, simple, lightweight and flexible React rendering solution that abstracts the complexities of server-side rendering. react-fetch-ssr focuses solely on requests to the API, the architecture of your application will remain in your complete control.
+          </p>
+        )}
         <div className={style.actions_content}>
           <a className={style.actions} href="https://github.com/armandojes/react-fetch-ssr">
             <img src={`${ASSETS}/icon/github.png`} alt=""/>  Github
           </a>
-          <a className={style.actions} href="https://github.com/armandojes/react-fetch-ssr">
-            <img src={`${ASSETS}/icon/github.png`} alt=""/>  Github
+          <a className={style.actions} href="https://react-fetch-ssr.now.sh">
+            <img src={`${ASSETS}/icon/web.png`} alt="web docs"/>  Docs
           </a>
         </div>
       </div>
