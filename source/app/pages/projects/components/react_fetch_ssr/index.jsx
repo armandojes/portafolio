023f13f1874fb87d397e3bc3a04590c7211e85ad
Jesusmaster <armandodejesus678@gmail.com>
2020-01-08 19:16:13 -0600

    integracion de react-fetch-ssr

--- source/app/pages/projects/components/react_fetch_ssr/index.jsx
+++ source/app/pages/projects/components/react_fetch_ssr/index.jsx
@@ -3,17 +3,20 @@ import style from './style';
 
 function ReactFetchSsr () {
   return (
-    <div className={style.content}>
+    <div className={style.content} style={{backgroundImage: `url(${ASSETS}/icon/react_gray.png)`}}>
       <div className={style.data}>
-        <div>
+        <h1>
           react-fetch-ssr
-        </div>
+        </h1>
         <p>
         react-fetch-ssr una solución de renderizado de React del lado del servidor, simple, liviana y flexible que abstrae las complejidades del renderizado del lado del servidor. react-fetch-ssr se enfoca únicamente en las peticiones al API, la arquitectura de tu aplicación seguirá estando en tu completo control.
         </p>
         <div className={style.actions_content}>
-          <a href="https://github.com/armandojes/react-fetch-ssr">
-            github
+          <a className={style.actions} href="https://github.com/armandojes/react-fetch-ssr">
+            <img src={`${ASSETS}/icon/github.png`} alt=""/>  Github
+          </a>
+          <a className={style.actions} href="https://github.com/armandojes/react-fetch-ssr">
+            <img src={`${ASSETS}/icon/github.png`} alt=""/>  Github
           </a>
         </div>
       </div>
